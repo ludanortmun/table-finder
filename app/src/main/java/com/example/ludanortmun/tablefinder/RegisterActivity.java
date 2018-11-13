021a535c4e0aa150fde07387e3dd9bd2fd549680
Daniel Ortega <danort.97@gmail.com>
2018-11-13 00:37:17 -0800

    Quick refactor of Activity class names

--- app/src/main/java/com/example/ludanortmun/tablefinder/RegisterActivity.java
+++ app/src/main/java/com/example/ludanortmun/tablefinder/RegisterActivity.java
@@ -3,11 +3,11 @@ package com.example.ludanortmun.tablefinder;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class activity_loginSuccess extends Activity {
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_success);
+        setContentView(R.layout.activity_register);
     }
 }
