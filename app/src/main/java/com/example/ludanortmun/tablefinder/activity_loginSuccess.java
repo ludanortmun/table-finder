ef6eafa4269c55da872c36522c77e5a323963e49
AragonJorge <aragon.jorge97@gmail.com>
2018-11-13 00:29:01 -0800

    Added Login Success & Register screen
    Title

---
+++ app/src/main/java/com/example/ludanortmun/tablefinder/activity_loginSuccess.java
@@ -0,0 +1,13 @@
+package com.example.ludanortmun.tablefinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_loginSuccess extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_success);
+    }
+}
